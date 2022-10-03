733addc767a4c47797c5108c3841ebf8221f7645
Catalina Villouta <catalina.villouta.r@gmail.com>
2022-10-02 18:03:40 -0700

    Delete duplicated queries

--- db/tiingo/00_create_table_assets_tiingo.sql
+++ db/tiingo/00_create_table_assets_tiingo.sql
@@ -6,6 +6,7 @@ CREATE TABLE assets_tiingo
 	ticker varchar(20) NOT NULL,
 	exchange varchar(20) NOT NULL,
 	asset_type varchar(20) NOT NULL,
+	currency varchar(5) NOT NULL,
 	start_date date NULL,
 	end_date date NULL,
 	lud timestamp without time zone NOT NULL,
