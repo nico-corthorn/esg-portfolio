b1cef53c26032bc333254a1956f4c89f2387d165
Nicolas Corthorn <nicolas.corthorn@gmail.com>
2022-11-14 22:32:08 -0800

    Generalize AlphaTableBalance into AlphaTableAccounting that addresses both Balance and Income

--- esgtools/main.py
+++ esgtools/main.py
@@ -6,15 +6,29 @@ from esgtools.etl import etl_alpha_api, etl_alpha_table
 def main():
     print("Starting scraper!")
     alpha_scraper = etl_alpha_api.AlphaScraper()
-    prices_keys = ["symbol", "date"]  # change schema
-    balance_keys = ["symbol", "report_type", "report_date", "currency", "account_name"]
-    alpha_assets = etl_alpha_table.AlphaTableAssets("assets_alpha", [], alpha_scraper)
-    alpha_prices = etl_alpha_table.AlphaTablePrices("prices_alpha", prices_keys, alpha_scraper)
-    alpha_balance = etl_alpha_table.AlphaTableBalance("balance_alpha", balance_keys, alpha_scraper)
 
-    size = 'compact'
-    alpha_prices.update("AAPL", size=size)
-    alpha_balance.update("AAPL")
+    # Assets
+    #alpha_assets = etl_alpha_table.AlphaTableAssets("assets_alpha", [], alpha_scraper)
+
+    # Prices
+    #prices_keys = ["symbol", "date"]  # change schema
+    #alpha_prices = etl_alpha_table.AlphaTablePrices("prices_alpha", prices_keys, alpha_scraper)
+    #size = 'compact'
+    #alpha_prices.update("AAPL", size=size)
+
+    # Accounting tables
+    accounting_keys = ["symbol", "report_type", "report_date", "currency", "account_name"]
+
+    # Balance
+    #balance_accounts = ['totalAssets', 'commonStock', 'commonStockSharesOutstanding']
+    #alpha_balance = etl_alpha_table.AlphaTableAccounting("balance_alpha", "BALANCE_SHEET", accounting_keys, alpha_scraper, balance_accounts, max_workers=6)
+    #alpha_balance.update("AAPL")
+
+    # Income
+    income_accounts = ['netIncome']
+    alpha_income = etl_alpha_table.AlphaTableAccounting("income_alpha", "INCOME_STATEMENT", accounting_keys, alpha_scraper, income_accounts)
+    #alpha_income.update("AAPL")
+    alpha_income.update_all()
 
     print("Done!")
 
