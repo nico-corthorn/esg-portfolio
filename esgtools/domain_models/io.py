cf6e96772bb7a6d2dc32c376c30ecf81301daed2
Nicolas Corthorn <nicolas.corthorn@gmail.com>
2024-11-10 23:02:01 -0800

    IO: Add domain_models.io

---
+++ esgtools/domain_models/io.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class SQLParams:
+    """Credentials to an RDS database."""
+
+    dbname: str
+    username: str
+    password: str
+    host: str
+    port: int
+
+
+def convert_dict_to_sql_params(db_credentials: dict) -> SQLParams:
+    filtered_db_credentials = {
+        key: db_credentials[key]
+        for key in SQLParams.__annotations__
+        if key in db_credentials
+    }
+    sql_params = SQLParams(**filtered_db_credentials)
+    return sql_params
